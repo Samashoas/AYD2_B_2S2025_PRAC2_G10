59d53ec3fd58e51c824e42be7d80b00d03330087
LuisAntoniokh <3016250180101@ingenieria.usac.edu.gt>
2025-10-22 11:21:59 -0600

    202109750: (feat) Backend de Servicio de Categoría
    Se agregó el servicio de la categoría. Posteriormente se añadirá el de los productos

---
+++ Backend/categories-service/app/models.py
@@ -0,0 +1,17 @@
+from app.database import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    description = db.Column(db.Text)
+    active = db.Column(db.Boolean, default=True)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'active': self.active
+        }
